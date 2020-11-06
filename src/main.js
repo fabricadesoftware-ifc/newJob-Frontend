e76481d9606c74509368496135a763e66d0173e0
beckhauser <beckhauser125@gmail.com>
2020-11-06 13:23:12 -0300

    feat: Adicionado icones na aplicação.

--- src/main.js
+++ src/main.js
@@ -5,11 +5,23 @@ import router from './router'
 import store from './store'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-Vue.use(Buefy, {
-  defaultIconPack: 'fas',
-  defaultContainerElement: '#content'
-})
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(Buefy, { defaultIconPack: 'fas' })
+
+// Vue.use(Buefy, {
+//   defaultIconComponent: 'vue-fontawesome',
+//   defaultIconPack: 'fas'
+// })
+
+// Vue.use(Buefy, {
+//   defaultIconPack: 'fas',
+//   defaultContainerElement: '#content'
+// })
 
 Vue.config.productionTip = false
 
