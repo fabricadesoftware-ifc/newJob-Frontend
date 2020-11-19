04bf22810823460ea1ca9ebf44c2c386cf3a1476
Lucas Alexandre Beckhauser <38123030+Beckhauser@users.noreply.github.com>
2020-11-19 16:20:43 -0300

    feat: Criando componente Hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,29 @@
+'<template>
+  <section class="hero primary is-fullheight-with-navbar">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title">{{ title }}</h1>
+        <p>{{ msg }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  components: {},
+  props: {
+    msg: String,
+    title: String
+  }
+}
+</script>
+
+<style>
+.primary {
+  background-image: url('http://videira.ifc.edu.br/wp-content/uploads/2019/05/42-38460006910_84e5a9ec99_b.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
