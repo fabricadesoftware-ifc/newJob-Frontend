bfc6206958f88f0c58d5ccb28e4c0a6aa76d19e6
Lucas Alexandre Beckhauser <38123030+Beckhauser@users.noreply.github.com>
2020-11-23 22:25:18 -0300

    style: Adicionando novos elementos a página principal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,27 @@
 '<template>
   <div class="home">
-    <!-- <Carousel /> -->
-    <div class="container mt-4">
-      <CardGrid />
-    </div>
+    <Hero
+      msg="Eos aperiam in non facilis vitae repellendus sequi vel molestiae. Quo voluptas facere fugiat adipisci molestiae quia. Eum inventore non. Itaque ut aliquid et rerum. Nihil fugit aut qui."
+      title="New Job AgriBusiness"
+    />
+    <Tiles class="mt-3" />
+    <Supporters class="mb-5" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Carousel from '@/components/Carousel.vue'
-import CardGrid from '@/components/CardGrid.vue'
-
+import Hero from '../components/Hero'
+import Contact from '../components/Contact'
+import Tiles from '../components/Tiles'
+import Supporters from '../components/Supporters.vue'
 export default {
   name: 'Home',
-  components: {
-    Carousel,
-    CardGrid
-  }
+  components: { Hero, Contact, Tiles, Supporters }
 }
 </script>
+
+<style>
+.home {
+  background-color: lightgray;
+}
+</style>
