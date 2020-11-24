482ddfc10d06999043f33dd6f2ab899b5e8c6640
Lucas Alexandre Beckhauser <38123030+Beckhauser@users.noreply.github.com>
2020-11-23 22:21:23 -0300

    feat: Adicionando Footer no App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
   <div id="app">
     <Navbar />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 export default {
-  components: { Navbar }
+  components: { Navbar, Footer }
 }
 </script>
 
