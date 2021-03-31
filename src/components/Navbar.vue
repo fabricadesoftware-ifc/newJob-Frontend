d753f468134c9d76a7bc3dd894aa3189f1be310f
Beckhauser <beckhaus_lucas@hotmail.com>
2021-03-31 15:54:36 -0300

    Componentizando Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,44 +3,36 @@
     <template slot="brand">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
         <img
+          class="navbar-image"
           src="https://cecom.ifc.edu.br/wp-content/uploads/sites/17/2015/10/Logo_IFC_horizontal.png"
           alt="Lightweight UI components for Vue.js based on Bulma"
         />
       </b-navbar-item>
     </template>
     <template slot="start">
-      <b-navbar-item href="#">
-        <router-link class="nav-text" to="/">Inicio</router-link>
-      </b-navbar-item>
-      <b-navbar-item href="#">
-        <router-link class="nav-text" to="/jobs">Vagas</router-link>
-      </b-navbar-item>
+      <NavbarItem :to="'/'">Inicio</NavbarItem>
+      <NavbarItem :to="'jobs'">Vagas</NavbarItem>
     </template>
 
     <template slot="end">
-      <b-navbar-item v-if="!token" tag="div">
-        <a class="button">
-          <router-link class="nav-text" to="/login">Entrar</router-link>
-        </a>
+      <!-- <router-link v-if="!token" class="nav-text" to="/login">
+        <b-navbar-item tag="div">
+          <a class="button">
+            <slot>Login</slot>
+          </a>
+        </b-navbar-item>
+      </router-link> -->
+
+      <NavbarItem v-if="!token" :to="'login'"
+        ><a class="button">Login</a></NavbarItem
+      >
+      <b-navbar-item v-else>
+        <NavbarDropDown />
       </b-navbar-item>
-      <a v-else @click="handleProfile">
-        <figure class="image is-32x32">
-          <img src="https://bulma.io/images/placeholders/128x128.png" />
-        </figure>
-      </a>
       <b-navbar-item tag="div">
         <a v-if="!token" class="button has-background-grey-lighter">
           <router-link class="nav-text" to="/signup"
             ><strong>Cadastre-se</strong></router-link
-          >
-        </a>
-        <a
-          v-else
-          class="button has-background-grey-lighter"
-          @click="handleLogout"
-        >
-          <router-link class="nav-text" to="/"
-            ><strong>Logout</strong></router-link
           >
         </a>
       </b-navbar-item>
@@ -51,33 +43,31 @@
 <script>
 // import { APIProfileGet } from '../services/api'
 import { mapGetters } from 'vuex'
+import NavbarItem from './NavbarItem'
+import NavbarDropDown from './NavbarDropDown'
 
 export default {
   computed: {
     ...mapGetters({ token: 'auth/hasToken' })
   },
+  components: { NavbarItem, NavbarDropDown },
   data() {
     return {
       //   token: false,
       picture: null
     }
   },
-  methods: {
-    handleLogout() {
-      localStorage.removeItem('token')
-      console.log('token removido')
-      this.$router.push({ name: 'Home' })
-    },
-    handleProfile() {
-      this.$router.push({ name: 'Profile' })
-    }
-  }
+  methods: {}
 }
 </script>
 
-<style scoped>
+<style>
 .nav-color {
   background-color: aquamarine;
+}
+
+.navbar-image {
+  size: 5px;
 }
 
 .nav-text {
---
+++ src/components/NavbarItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-navbar-item @click="$router.push(to)">
+    <slot></slot>
+  </b-navbar-item>
+</template>
+
+<script>
+export default {
+  name: 'NavbarButton',
+  props: {
+    to: String
+  }
+}
+</script>
+
+<style></style>
