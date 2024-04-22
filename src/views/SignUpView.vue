ffaa6ddaf3f26593ce9779f70f975f0d62a1d09a
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-04-22 17:59:32 -0300

    Criação das telas de cadastro.

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ImageForm, SignUpForm} from '@/components/index';
+</script>
+
+<template>
+    <div class="container">
+    <SignUpForm/>
+    <ImageForm side="right" image="https://i.ibb.co/ygh8VM8/image.png"/>
+    </div>
+</template>
+
+<style scoped>
+    .container{
+        display: flex;
+    }
+</style>
