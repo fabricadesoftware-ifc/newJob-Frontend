8dc6e505fa5b40e14fdd19ea56a4c9feed0cef03
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-04-22 18:01:44 -0300

    Correção no css e utilização do componente modificado.

--- src/components/login/LoginForm.vue
+++ src/components/login/LoginForm.vue
@@ -29,59 +29,69 @@ const showPassword = ref(false);
       </div>
     </label>
     <button>Login</button>
+    <p>Ainda não possui conta?
+      <router-link to="/signUp" class="link-text">Crie uma</router-link>
+    </p>
   </form>
 </template>
 
 <style scoped>
+.link-text {
+    color: #E09B6B;
+    cursor: pointer;
+}
+
 .container {
-  width: 50%;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  font-size: 1.2rem;
+    width: 50%;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    font-size: 1rem;
 }
 
 h1 {
-  margin-bottom: 3%;
+    margin-bottom: 3%;
 }
 
 button {
-  margin-top: 5%;
-  width: 40%;
-  background-color: #E09B6B;
-  border-radius: 3rem;
-  padding: 2% 0;
-  font-size: 1.2rem;
-  color: #000000;
-  font-weight: 600;
+    margin-top: 5%;
+    width: 50%;
+    background-color: #E09B6B;
+    border-radius: 3rem;
+    padding: 2% 0;
+    font-size: 1rem;
+    color: #000000;
+    font-weight: 600;
 }
 
 input {
-  width: 90%;
-  margin-bottom: .3rem;
-  font-size: 1.2rem;
-  color: #716F6F ;
-  background-color: transparent;
-  outline: none;
-  border: none;
-  
+    width: 90%;
+    margin-bottom: .3rem;
+    font-size: 1rem;
+    color: #716F6F;
+    background-color: transparent;
+    outline: none;
+    border: none;
 }
-label{
-  width: 40%;
+
+label {
+    width: 50%;
 }
+
 .input-container {
-  color: #716F6F ;
-  border: 1px solid #716F6F;
-  border-radius: .6rem;
-  padding: 1.4rem .5rem 1rem .5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 2%;
+    color: #716F6F;
+    border: 1px solid #716F6F;
+    border-radius: .6rem;
+    padding: 1rem .5rem .7rem .5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 2%;
 }
-.input-container:focus-within{
-  border: 1px solid #E09B6B;
-  transition: all .2s ease-in-out ;
-  
+
+.input-container:focus-within {
+    border: 1px solid #E09B6B;
+    transition: all .2s ease-in-out;
 }
 </style>
