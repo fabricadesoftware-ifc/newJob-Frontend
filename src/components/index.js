13ee5e78bdf66f09dc806904c205934cd15e00ba
GabrielLima2803 <gabriellima2803@gmail.com>
2024-04-23 15:22:10 -0300

    feat: Adicionando os components no export

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import ImageForm from "./login/ImageForm.vue";
 import LoginForm from "./login/LoginForm.vue";
+import LoadingPage from "./loading/LoadingPage.vue";
+import FooterComponent from "./footer/FooterComponent.vue";
+import HeaderComponent from "./header/HeaderComponent.vue";
 
 export {
     ImageForm,
-    LoginForm
+    LoginForm,
+    LoadingPage,
+    FooterComponent,
+    HeaderComponent,
 }
