bca8654d3d5907400ffb89ce31d9daea018f7407
GabrielLima2803 <gabriellima2803@gmail.com>
2024-04-23 15:53:20 -0300

    style: correção de estilo por causa da resolução

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -1,61 +1,66 @@
-<script setup>
-
-</script>
-
 <template>
-    <div class="container-header">
-        <div class="container-logo">
-            <img src="https://i.ibb.co/6g5QTs3/logo-newjob-png.png" alt="" class="logo">
-        </div>
-        <div class="flex box-mid">
-            <p class="border">Início</p>
-            <p class="padding-fix">Vagas</p>
-        </div>
-        <div class="flex box-mid">
-            <p class="padding-fix">Login</p>
-            <p class="register">Get started</p>
-        </div>
+  <div class="container-header" style="padding-top: 40px">
+    <div class="container-logo">
+      <img src="https://i.ibb.co/6g5QTs3/logo-newjob-png.png" alt="" class="logo" />
     </div>
+    <div class="flex box-mid">
+      <p class="border">Início</p>
+      <p class="padding-fix">Vagas</p>
+    </div>
+    <div class="flex box-mid">
+      <p class="padding-fix">Login</p>
+      <p class="register">Get started</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.container-header{
-    display: flex;
-    justify-content: space-evenly; 
-    /* justify-content: space-around;  */
-    /* background-color: transparent; Talvez isso funcione dps */
-    padding: 2.5em 1em 1em 1em;
+.container-header {
+  display: flex;
+  justify-content: space-around;
+  background-color: transparent;
+  padding: 2.5em 1em;
 }
-.flex{
-    display: flex;
-    gap: 0.5em;
+
+.flex {
+  display: flex;
+  gap: 0.5em;
 }
-.logo{
-    width: 35%;
+
+.container-logo {
+  width: auto;
 }
-.box-mid{
-    background-color: white;
-    border-radius: 5em;
-    padding: 0.5em 1em 0.5em 1em;
-   
+
+.logo {
+  width: 10.5em;
 }
+
+.box-mid {
+  background-color: white;
+  border-radius: 5em;
+  padding: 0.5em 1em 0.5em 1em;
+}
+
 .box-mid > p {
-    color: black;
-    font-size: 15px;
-    cursor: pointer;
+  color: black;
+  font-size: 15px;
+  cursor: pointer;
 }
-.border{
-    border: solid 1.5px black;
-    border-radius: 5em;
-    padding: 0.3em 1em 0.3em 1em;
+
+.border {
+  border: solid 1.5px black;
+  border-radius: 5em;
+  padding: 0.3em 1em 0.3em 1em;
 }
-.padding-fix{
-    padding: 0.3em 0.7em 0.3em 0.7em;
+
+.padding-fix {
+  padding: 0.3em 0.7em 0.3em 0.7em;
 }
-.register{
-    background-color: #1D4734;
-    color: white !important;
-    padding: 0.3em 0.7em 0.3em 0.7em;
-    border-radius: 5em;
+
+.register {
+  background-color: #1d4734;
+  color: white !important;
+  padding: 0.3em 0.7em 0.3em 0.7em;
+  border-radius: 5em;
 }
 </style>
