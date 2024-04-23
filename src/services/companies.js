78a37dfc0ba1bc3376dbdb39dc3a292297e8b96d
GabrielLima2803 <gabriellima2803@gmail.com>
2024-04-23 15:22:42 -0300

    fix: import do plugin

--- src/services/companies.js
+++ src/services/companies.js
@@ -1,4 +1,5 @@
-import api from '@/plugins/axios';
+import { api } from '@/plugins/axios';
+
 class CompanyService {
     async getAllCompanies() {
         const response = await api.get('companies/')
