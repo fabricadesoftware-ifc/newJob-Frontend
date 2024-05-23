7de2fc08188f6788a55a0bc83057f168e809c591
GabrielLima2803 <gabriellima2803@gmail.com>
2024-05-23 12:12:53 -0300

    feat: Adição do component na exportação principal

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import LoginForm from "./login/LoginForm.vue";
 import SignUpForm from "./signUp/SignUpForm.vue";
 import IntroductionJobs from "./jobPage/IntroductionJobs.vue";
 import MainContentJobs from "./jobPage/MainContentJobs.vue";
+import FooterComponent from "./footer/FooterComponent.vue";
 
 export {
     ImageForm,
     LoginForm,
     SignUpForm,
     IntroductionJobs,
-    MainContentJobs
+    MainContentJobs,
+    FooterComponent
 }
