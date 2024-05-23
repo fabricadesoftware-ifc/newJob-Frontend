159eb7fb875cce08f119b6576abb4b7b08908410
GabrielLima2803 <gabriellima2803@gmail.com>
2024-05-23 05:41:27 -0300

    feat: Criação dos components da pagina Job

---
+++ src/components/jobPage/IntroductionJobs.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderComponent from "../header/HeaderComponent.vue";
+</script>
+
+<template>
+  <div class="container">
+    <HeaderComponent/>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  background-color: #1D4734;
+  padding: 100px;
+}
+</style>
---
+++ src/components/jobPage/MainContentJobs.vue
@@ -0,0 +1,43 @@
+<script setup>
+import CardJobs from './SecondComponents/CardJobs.vue';
+</script>
+
+<template>
+  <div class="container">
+    <div class="title-job">
+      <img src="https://i.ibb.co/mbNR5y4/Group-1171274710.png" alt="" class="img-title" />
+      <h2>Vagas</h2>
+    </div>
+    <div class="container-card" >
+        <div class="card" v-for="index in 4" :key="index" >
+            <CardJobs />
+        </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.title-job {
+  display: flex;
+  gap: 1em;
+}
+.title-job > h2 {
+  font-weight: normal;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.img-title {
+  width: 80px;
+}
+.container-card {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.card {
+  flex-basis: calc(50% - 1em); /* Largura de 50% com margem de 1em */
+  margin-bottom: 1em; 
+}
+</style>
