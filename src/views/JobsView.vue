2fdc1f24943d9e596becc6466a01de2d4189fbc5
GabrielLima2803 <gabriellima2803@gmail.com>
2024-05-23 05:41:51 -0300

    feat: Criação da view

---
+++ src/views/JobsView.vue
@@ -0,0 +1,18 @@
+<script setup>
+// import CardJobs from '@/components/jobPage/SecondComponents/CardJobs.vue';
+// import { IntroductionJobs } from '@/components';
+import { MainContentJobs } from "@/components";
+
+</script>
+
+<template>
+  <div>
+    <!-- <IntroductionJobs/> -->
+    <!-- <CardJobs/> -->
+    <MainContentJobs/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
