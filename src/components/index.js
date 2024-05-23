9de2691e9f32057f48e5323ab34e1dcdb5122915
GabrielLima2803 <gabriellima2803@gmail.com>
2024-05-23 05:41:04 -0300

    feat: Adição de icones

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,13 @@
 import ImageForm from "./ImageForm.vue";
 import LoginForm from "./login/LoginForm.vue";
 import SignUpForm from "./signUp/SignUpForm.vue";
+import IntroductionJobs from "./jobPage/IntroductionJobs.vue";
+import MainContentJobs from "./jobPage/MainContentJobs.vue";
 
 export {
     ImageForm,
     LoginForm,
-    SignUpForm
+    SignUpForm,
+    IntroductionJobs,
+    MainContentJobs
 }
