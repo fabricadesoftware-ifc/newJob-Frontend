61b1f760c9c09cb56cc5471707a01b5ab2b8d9b1
GabrielLima2803 <gabriellima2803@gmail.com>
2024-07-04 19:07:18 -0300

    test: branch de test para criação de um job

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import LoginView from '../views/LoginView.vue'
 import SignUpView from '../views/SignUpView.vue'
 import JobsView from '@/views/JobsView.vue'
 import AboutView from '@/views/AboutView.vue'
+import TesteJobView from '@/views/TesteJobView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    },
+    {
+      path: '/test-job',
+      name: "test-job",
+      component: TesteJobView
     }
   ]
 })
