eebecf7fee27da08bf25408798805a6874cb702f
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-09-05 14:14:18 -0300

    FEAT: Criação da services e da store para armazenar a vaga atual.

--- src/services/jobs.js
+++ src/services/jobs.js
@@ -1,19 +1,23 @@
 import axios from "axios";
 
 class JobService {
-    async getAllJobs(){
+    async getAllJobs() {
         const response = await axios.get('job/')
         return response.data
     }
-    async deleteJob(id){
+    async getJob(id) {
+        const response = await axios.get(`job/${id}`)
+        return response.data
+    }
+    async deleteJob(id) {
         const response = await axios.delete(`job/${id}`)
         return response.data
     }
-    async createJob(jobData){
+    async createJob(jobData) {
         const response = await axios.post(`job/`, jobData)
         return response.data
     }
-    async updateJob(id, jobData){
+    async updateJob(id, jobData) {
         const response = await axios.put(`job/${id}/`, jobData)
         return response.data
     }
--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -2,20 +2,26 @@ import { defineStore } from "pinia";
 import { computed, reactive } from "vue";
 import JobService from "@/services/jobs";
 
-
 export const useJobStore = defineStore('job', () => {
     const state = reactive({
-        jobs: []
+        jobs: [],
+        currentJob: []
     })
-    const jobs = computed(()=> state.jobs)
+    const jobs = computed(() => state.jobs)
+    const currentJob = computed(() => state.currentJob)
 
     const getAllJobs = async () => {
         const data = await JobService.getAllJobs()
         state.jobs = data
     }
 
+    const getJob = async (id) => {
+        const data = await JobService.getJob(id)
+        state.currentJob = data
+    }
+
     const createJob = async (jobData) => {
-        const newJob  = await JobService.createJob(jobData)
+        const newJob = await JobService.createJob(jobData)
         state.jobs.push(newJob);
     }
 
@@ -26,13 +32,12 @@ export const useJobStore = defineStore('job', () => {
 
     const updateJob = async (id, jobData) => {
 
-            const updatedJob = await JobService.updateJob(id, jobData);
-            const index = state.jobs.findIndex(job => job.id === id);
-            if (index !== -1) {
-                state.jobs[index] = updatedJob;
-            }
-
+        const updatedJob = await JobService.updateJob(id, jobData);
+        const index = state.jobs.findIndex(job => job.id === id);
+        if (index !== -1) {
+            state.jobs[index] = updatedJob;
+        }
     };
 
-    return {jobs, getAllJobs, createJob, deleteJob, updateJob}
-})
+    return { jobs, currentJob, getAllJobs, getJob, createJob, deleteJob, updateJob }
+})      
