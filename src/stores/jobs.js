87503eb5b73eb8804a73d3416cc9669b930ffb99
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-09-11 09:31:34 -0300

    FEAT: Utilizando useStorage para armazenar as vagas.

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -1,41 +1,42 @@
 import { defineStore } from "pinia";
-import { computed, reactive } from "vue";
+import { computed } from "vue";
+import { useStorage } from "@vueuse/core"
 import JobService from "@/services/jobs";
 
 export const useJobStore = defineStore('job', () => {
-    const state = reactive({
+    const state = useStorage("jobs", {
         jobs: [],
         currentJob: []
     })
-    const jobs = computed(() => state.jobs)
-    const currentJob = computed(() => state.currentJob)
+    const jobs = computed(() => state.value.jobs)
+    const currentJob = computed(() => state.value.currentJob)
 
     const getAllJobs = async () => {
         const data = await JobService.getAllJobs()
-        state.jobs = data
+        state.value.jobs = data
     }
 
     const getJob = async (id) => {
         const data = await JobService.getJob(id)
-        state.currentJob = data
+        state.value.currentJob = data
     }
 
     const createJob = async (jobData) => {
         const newJob = await JobService.createJob(jobData)
-        state.jobs.push(newJob);
+        state.value.jobs.push(newJob);
     }
 
     const deleteJob = async (id) => {
         await JobService.deleteJob(id);
-        state.jobs = state.jobs.filter(job => job.id !== id);
+        state.value.jobs = state.value.jobs.filter(job => job.id !== id);
     };
 
     const updateJob = async (id, jobData) => {
 
         const updatedJob = await JobService.updateJob(id, jobData);
-        const index = state.jobs.findIndex(job => job.id === id);
+        const index = state.value.jobs.findIndex(job => job.id === id);
         if (index !== -1) {
-            state.jobs[index] = updatedJob;
+            state.value.jobs[index] = updatedJob;
         }
     };
 
