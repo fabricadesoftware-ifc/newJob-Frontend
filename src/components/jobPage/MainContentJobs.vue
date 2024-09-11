fe141945537bca969c1865ecb729eef353d6bde7
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-09-11 09:37:36 -0300

    FEAT: Alpicando carrossel para listagem de vagas.

--- src/components/jobPage/MainContentJobs.vue
+++ src/components/jobPage/MainContentJobs.vue
@@ -3,7 +3,11 @@ import { onMounted } from "vue";
 import CardJobs from "./SecondComponents/CardJobs.vue";
 import { ArrowLeftThinCircleOutline, ArrowRightThinCircleOutline } from "../icons";
 import { useJobStore } from "@/stores";
+import { ref } from "vue";
 const jobStore = useJobStore();
+
+const firstSliceNum = ref(0)
+const secSliceNum = ref(4)
 
 async function getJobs() {
   await jobStore.getAllJobs();
@@ -16,6 +20,16 @@ async function getJob(id) {
 onMounted(async () => {
   await getJobs();
 });
+
+function nextJobs() {
+  firstSliceNum.value += 4;
+  secSliceNum.value += 4;
+}
+
+function previousJobs() {
+  firstSliceNum.value -= 4;
+  secSliceNum.value -= 4;
+}
 </script>
 
 <template>
@@ -25,13 +39,14 @@ onMounted(async () => {
       <h2>Vagas</h2>
     </div>
     <div class="grid-container">
-      <div class="container-card" v-for="job in jobStore.jobs" :key="job">
+      <div class="container-card" v-for="job in jobStore.jobs.slice(firstSliceNum, secSliceNum)" :key="job">
         <CardJobs :company="job.company.name" :title="job.title" :local="job.local" :id="job.id" :getJob="getJob" />
       </div>
     </div>
     <div class="buttons">
-      <ArrowLeftThinCircleOutline class="arrow" />
-      <ArrowRightThinCircleOutline class="arrow" />
+      <ArrowLeftThinCircleOutline @click="previousJobs()" :class="firstSliceNum == 0 ? `disable arrow` : `arrow`" />
+      <ArrowRightThinCircleOutline @click="nextJobs()"
+        :class="secSliceNum >= jobStore.jobs.length ? `disable arrow` : `arrow`" />
     </div>
   </div>
 </template>
@@ -81,6 +96,11 @@ onMounted(async () => {
   scale: 2;
   color: white;
   padding: 5em 0px 0px 0px;
+}
+
+.disable {
+  pointer-events: none;
+  filter: opacity(.5);
 }
 
 .arrow {
