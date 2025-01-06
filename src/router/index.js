7791c385809201657fb7f5c66e99425fbc8f15ca
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2025-01-06 15:36:46 -0300

    FEAT: Criação da view de usuário

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import LoginView from '../views/LoginView.vue'
 import SignUpView from '../views/SignUpView.vue'
 import JobsView from '@/views/JobsView.vue'
 import AboutView from '@/views/AboutView.vue'
+import ProfileView from '@/views/ProfileView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +34,11 @@ const router = createRouter({
       path: '/about/:id',
       name: 'about',
       component: AboutView
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView
     },
   ]
 })
