9f08c0de0378d06dcd38cc9b4838c18aa1f251ed
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2025-01-15 13:44:15 -0300

    CHORE: Criação dos componentes e barra de pesquisa estática

---
+++ src/components/candidates/CandidadesContent.vue
@@ -0,0 +1,31 @@
+<script setup>
+import ListComponent from "./candidatesComponents/ListComponent.vue";
+import SearchComponent from "./candidatesComponents/SearchComponent.vue";
+import UserComponent from "./candidatesComponents/UserComponent.vue";
+</script>
+
+<template>
+  <main>
+  <h1>Candidatos</h1>
+      <SearchComponent />
+    <div class="content-container">
+      <ListComponent />
+      <UserComponent />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main{
+ max-width: 100vw;
+ padding: 15vh 2vw;
+}
+
+h1{
+    margin-bottom: 5vh;
+}
+
+.content-container {
+    display: flex;
+}
+</style>
---
+++ src/components/candidates/candidatesComponents/SearchComponent.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { Magnify } from "@/components/icons";
+</script>
+
+<template>
+  <div class="container">
+    <label>
+      <Magnify />
+      <input type="text" />
+    </label>
+    <button>Todos</button>
+    <button>Favoritos</button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  gap: 1vw;
+}
+
+label {
+  display: flex;
+  width: 25vw;
+  min-width: 200px;
+  height: 5vh;
+  min-height: 20px;
+  background-color: #d9d9d9;
+  align-items: center;
+  border-radius: 16px;
+  padding: 0 0.5vw;
+  color: #716f6f;
+
+  & span {
+    display: flex;
+  }
+
+  & input {
+    width: 100%;
+    background-color: transparent;
+    border: none;
+    outline: none;
+  }
+}
+
+button{
+    padding: .5vh 1.5vw;
+    background-color: #1D4734;
+    color: #d9d9d9;
+    border: none;
+    border-radius: 16px;
+}
+</style>
