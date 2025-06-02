5a5007f8431167a8d37e2059b78ab70ebeaf90e5
IsadoraAlcantara <isadoraalca405@gmail.com>
2025-06-01 22:20:20 -0300

    CHORE: criando o interior do dropdown de filtro

--- src/components/jobPage/MainContentJobs.vue
+++ src/components/jobPage/MainContentJobs.vue
@@ -20,6 +20,17 @@ async function changeJobs(page) {
   <div class="container">
     <h1>VAGAS</h1>
     <FilterJobs />
+
+    <div class="filter-dropdown-in">
+      <div class="tilte-container">
+        <div class="title">
+          <FilterVariant />
+          <h2>Filtro</h2>
+        </div>
+        <Close />
+      </div>
+    </div>
+
     <div class="mini-company-cards-container">
       <div v-for="n in 13" :key="n">
         <MiniCompanyCard />
@@ -61,6 +72,32 @@ async function changeJobs(page) {
 </template>
 
 <style scoped>
+.filter-dropdown-in {
+  border: 1px solid var(--cinza-escuro);
+  border-radius: 10px;
+  color: var(--branco);
+
+  & .tilte-container {
+    display: flex;
+    justify-content: center;
+
+    & span {
+      display: flex;
+      align-items: center;
+    }
+
+    & .title {
+      display: flex;
+      gap: 5px;
+
+      & h2 {
+        font-size: 18px;
+        font-weight: 500;
+      }
+    }
+  }
+}
+
 .container {
   padding: 8em;
 }
