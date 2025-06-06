6cb1baaa074f2e3865a01a06a319eb3acb3bfa55
Isadoraalcantara <Isadoraalca405@gmail.com>
2025-06-06 08:23:23 -0300

    CHORE: componentização e estilização do componente select na página de vagas

--- src/components/jobPage/SecondComponents/FIlterJobsDropdownComponent.vue
+++ src/components/jobPage/SecondComponents/FIlterJobsDropdownComponent.vue
@@ -1,10 +1,6 @@
 <script setup>
-import { ref } from 'vue'
-import { FilterVariant, Close, ChevronDown, ChevronUp } from '../../icons'
-
-const lista = ['PJ', 'Estágio', 'CLT']
-
-const showIcon = ref(false)
+import { FilterVariant, Close } from '../../icons'
+import { SelectComponent } from '@/components'
 </script>
 <template>
   <div class="filter-jobs-container">
@@ -15,87 +11,10 @@ const showIcon = ref(false)
       </div>
       <Close size="22px" />
     </div>
-    <div class="hr"></div>
-    <div class="select-container">
-      <div class="title-select">
-        <p>Tipo de contrato</p>
-        <p style="color: var(--laranja)">Descartar</p>
-      </div>
-      <select name="expired" id="expired">
-        <option value="teste">Teste</option>
-        <option value="teste">Teste</option>
-        <option value="teste">Teste</option>
-      </select>
-    </div>
-    <div class="select">
-      <div id="category-select">
-        <label for="options-view-button">Categoria</label>
-        <input type="checkbox" id="options-view-button" />
-      </div>
-
-      <div class="select-button" @click="showIcon = !showIcon">
-        <div class="selected-value">Todos</div>
-        <div class="chevrons">
-          <ChevronDown size="20" v-if="!showIcon" />
-          <ChevronUp size="20" v-if="showIcon" />
-        </div>
-      </div>
-      <div class="options-container" v-if="showIcon">
-        <div class="option" v-for="opcao in lista">
-          {{ opcao }}
-        </div>
-      </div>
-    </div>
+    <SelectComponent />
   </div>
 </template>
 <style scoped>
-label {
-  font-size: 14px;
-}
-
-.select-button,
-.options-container {
-  border: 1px solid var(--cinza-claro);
-  margin-top: 0.3rem;
-  border-radius: 0.375rem;
-  padding: 0.4rem 0.6rem;
-  font-size: 15px;
-}
-
-.select-button {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  color: var(--cinza-claro);
-
-  & .selected-value {
-    font-size: 0.875rem;
-  }
-
-  & .chevrons span {
-    display: flex;
-    align-items: center;
-  }
-}
-
-.options-container {
-  padding: 0.2rem;
-  z-index: 4;
-  position: absolute;
-  width: 292px;
-  background-color: var(--preto);
-}
-
-.option {
-  padding: 0.3rem 0.6rem;
-  border-radius: 0.2rem;
-  font-size: 13px;
-}
-
-.option:hover {
-  background-color: #393939;
-}
-
 .filter-jobs-container {
   display: flex;
   flex-direction: column;
@@ -135,36 +54,6 @@ label {
   background-color: var(--cinza-escuro);
   width: 100%;
   height: 1px;
-}
-
-.select-container {
-  display: flex;
-  flex-direction: column;
-  gap: 7px;
-
-  & .title-select {
-    display: flex;
-    justify-content: space-between;
-    font-size: 14px;
-  }
-
-  & select {
-    width: 100%;
-    padding: 6px;
-    border-radius: 5px;
-    border: 1px solid var(--cinza-claro);
-    background-color: transparent;
-    color: var(--cinza-claro);
-    font-size: 14px;
-
-    & option {
-      background-color: var(--preto);
-    }
-
-    & option:hover {
-      background-color: #393939;
-    }
-  }
 }
 
 .button-container {
---
+++ src/components/jobPage/SecondComponents/SelectComponent.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { ref } from 'vue'
+import { ChevronDown, ChevronUp } from '../../icons'
+const lista = ['PJ', 'Estágio', 'CLT']
+
+const showIcon = ref(false)
+</script>
+<template>
+  <div class="select-container">
+    <div class="title-container">
+      <p>Tipo de contrato</p>
+      <p style="color: var(--laranja)">Descartar</p>
+    </div>
+    <div class="select" @click="showIcon = !showIcon">
+      <div class="selected-value">Todos</div>
+      <div class="chevrons">
+        <ChevronDown size="20" v-if="!showIcon" />
+        <ChevronUp size="20" v-if="showIcon" />
+      </div>
+    </div>
+    <div class="options-container" v-if="showIcon">
+      <div class="option" v-for="opcao in lista">
+        {{ opcao }}
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.select-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+
+  & .title-container {
+    display: flex;
+    justify-content: space-between;
+    font-size: 14px;
+  }
+
+  & .select {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: var(--cinza-claro);
+
+    & .selected-value {
+      font-size: 0.875rem;
+    }
+
+    & .chevrons span {
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  & .options-container {
+    padding: 0.2rem;
+    z-index: 4;
+    position: absolute;
+    top: 70px;
+    width: 292px;
+    background-color: var(--preto);
+
+    & .option {
+      padding: 0.4rem 0.6rem;
+      border-radius: 0.2rem;
+      font-size: 13px;
+    }
+    & .option:hover {
+      background-color: #393939;
+    }
+  }
+}
+
+.select,
+.options-container {
+  border: 1px solid var(--cinza-claro);
+  margin-top: 0.3rem;
+  border-radius: 0.375rem;
+  padding: 0.4rem 0.6rem;
+  font-size: 15px;
+}
+</style>
