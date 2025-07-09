dd0ff4646174a58ebe4fd1114b367e4ca2f5c828
IsadoraAlcantara <IsadoraAlcantara>
2025-07-09 14:43:04 -0300

    CHORE: posicionando elementos no hero da home

--- src/components/homePage/PageIntroduction.vue
+++ src/components/homePage/PageIntroduction.vue
@@ -1,103 +1,123 @@
 <script setup>
-import { ArrowTopRight } from '../icons';
-
+import { ArrowTopRight } from '../icons'
 </script>
 
 <template>
-    <div class="container">
-        <img class="bg-img" src="https://i.ibb.co/6gZqqxG/line-bg.png" alt="">
-        <div class="image-container">
-            <span class="main-image">
-            </span>
-            <button class="image-btn">
-                <ArrowTopRight size="60" />
-            </button>
-        </div>
-        <div class="content-container">
-            <h1>VAGAS AGRÍCOLAS PARA DIVERAS HABILIDADES.</h1>
-            <p>New Job Agri Business é um software inovador projetado para revolucionar o setor de recrutamento
-                agrícola. Nosso objetivo é criar uma ponte entre profissionais talentosos e empresas agrícolas em busca
-                de mão de obra qualificada.</p>
-            <img src="https://i.ibb.co/f1cn1Rv/agri-logo.png" alt="">
-        </div>
+  <div class="container">
+    <img class="bg-img" src="https://i.ibb.co/6gZqqxG/line-bg.png" alt="" />
+    <div class="image-container">
+      <span class="main-image"> </span>
+      <div class="image-btn">
+        <ArrowTopRight size="100" />
+      </div>
     </div>
+    <div class="content-container">
+      <h1>VAGAS AGRÍCOLAS PARA DIVERAS HABILIDADES.</h1>
+      <p>
+        New Job Agri Business é um software inovador projetado para revolucionar o setor de
+        recrutamento agrícola. Nosso objetivo é criar uma ponte entre profissionais talentosos e
+        empresas agrícolas em busca de mão de obra qualificada.
+      </p>
+      <button class="button-show-more">Ver vagas</button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .container {
-    border-radius: 40px;
-    min-height: 100%;
-    background: linear-gradient(28deg, #0E301C 15%, #07734B, #0E301C 85%);
+  border-radius: 40px;
+  min-height: 100%;
+  background: linear-gradient(28deg, #0e301c 15%, #07734b, #0e301c 85%);
 }
 
-.content-container {
-    position: relative;
-    padding: 10% 0 0 5%;
+.bg-img {
+  position: absolute;
+  right: 3%;
+  filter: brightness(3) opacity(30%);
+  height: 90%;
 }
 
-.content-container img{
-    width: 10%;
-    margin: 2% 0;
-}
 
-p {
-    max-width: 30%;
-    color: var(--branco);
-    font-size: .9rem;
-}
+.image-container {
+  position: absolute;
+  right: 9%;
+  bottom: 14%;
+  width: 49%;
+  height: 56%;
 
-h1 {
-    color: var(--branco);
-    max-width: 80%;
-    font-size: 4rem;
-    font-weight: 600;
-}
+  & .main-image {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: 40em;
+    rotate: 6deg;
+    box-shadow:
+      0px -60px 70px 10px var(--preto-escuro) inset,
+      -10px 10px 0px #000000a1,
+      15px 10px 0px #000000a1;
+    background: url('https://i.ibb.co/RgKjgKf/image.png');
+    opacity: 80%;
+    background-size: cover;
+  }
 
-.image-btn {
+  & .image-btn {
     position: absolute;
     display: flex;
     justify-content: center;
     align-items: center;
     right: 5%;
     top: -5%;
-    background-color: var(--laranja);
-    border: none;
-    width: 6rem;
-    height: 6rem;
+    background: #b9672d;
+    background: linear-gradient(0deg, rgba(185, 103, 45, 1) 0%, rgba(224, 155, 107, 1) 50%);
+    width: 9rem;
+    height: 9rem;
     border-radius: 50%;
+
+    & span {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
 }
 
-.image-btn span {
-    translate: 0 10%;
-}
+.content-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2vw;
+  position: relative;
+  padding: 7% 0 0 8%;
+  width: 70%;
 
-.main-image {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    border-radius: 40em;
-    rotate: 6deg;
-    box-shadow: 0px -40px 70px 10px var(--preto-escuro) inset,
-        -10px 5px 0px #000000a1,
-        5px 5px 0px #000000a1;
-    background-color: var(--branco);
-    background: url("https://i.ibb.co/RgKjgKf/image.png");
-    opacity: 80%;
-    background-size: cover;
-}
+  & h1 {
+    color: var(--branco);
+    max-width: 80%;
+    font-size: 5.7rem;
+    font-weight: 600;
+    line-height: 1.2;
+  }
 
-.image-container {
-    position: absolute;
-    right: 6%;
-    bottom: 8%;
-    width: 50%;
-    height: 55%;
-}
+  & p {
+    max-width: 45%;
+    color: var(--branco);
+    font-size: 1.1rem;
+    word-spacing: 2px;
+  }
 
-.bg-img {
-    position: absolute;
-    right: 0;
-    filter: brightness(3) opacity(30%);
-    height: 90%;
+  & .button-show-more {
+  background: #b9672d;
+  background: linear-gradient(
+    90deg,
+    rgba(185, 103, 45, 1) 0%,
+    rgba(224, 155, 107, 1) 50%,
+    rgba(185, 103, 45, 1) 100%
+  );
+  border: none;
+  height: 2.5vw;
+  width: 12vw;
+  border-radius: 30px;
+  font-weight: 600;
+  font-size: 1.1em;
+}
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,33 +8,24 @@ import {
 </script>
 
 <template>
-  <div class="father-container">
-    <div class="teste">
-      <HeaderComponent class="header" />
-      <PageIntroduction class="introduction" />
-    </div>
-    <MainContent />
-    <FooterComponent />
+  <div class="teste">
+    <HeaderComponent class="header" />
+    <PageIntroduction class="introduction" />
   </div>
+  <MainContent />
+  <FooterComponent />
 </template>
 <style scoped>
-.father-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
 .teste {
   display: grid;
-  padding: 0 2vh 2vh 2vh;
-  max-height: 100vh;
-  height: 98vh;
+  padding: 0 2% 2% 2%;
+  height: 96vh;
   align-items: center;
-  justify-content: center;
-  grid-template-rows: 8% 92%;
+  
+  grid-template-rows: 6% auto;
   grid-template-areas:
-    "header"
-    "introduction";
+    'header'
+    'introduction';
 
   & .introduction {
     grid-area: introduction;
