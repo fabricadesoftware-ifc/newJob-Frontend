df311e7a58604b59464f108218dd46d1ae707ad2
IsadoraAlcantara <Isadoraalca405@gmail.com>
2025-09-04 19:37:25 -0300

    FEAT: criação do footer mobile (barra de navegação)

--- src/components/footer/Mobile/MobileFooter.vue
+++ src/components/footer/Mobile/MobileFooter.vue
@@ -1,3 +1,51 @@
 <template>
-    mobile footer
+  <div class="container">
+    <div class="cel">
+      <i class="mdi mdi-home-outline"></i>
+      <p>Início</p>
+    </div>
+    <div class="cel">
+      <i class="mdi mdi-briefcase-outline"></i>
+      <p>Vagas</p>
+    </div>
+    <div class="cel">
+      <img src="https://i.ibb.co/G4hJhmq2/image.png" alt="" />
+      <p>Perfil</p>
+    </div>
+  </div>
 </template>
+<style scoped>
+.container {
+  position: relative;
+  bottom: 0;
+  background-color: var(--preto);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 50px;
+  height: 49px;
+  border-radius: 20px 20px 0 0;
+}
+
+.cel {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: var(--cinza-claro);
+
+  & img {
+    width: 24px;
+    height: 24px;
+  }
+
+  & .mdi {
+    font-size: 24px;
+    height: 20px;
+  }
+
+  & p {
+    color: var(--branco-escuro);
+    font-size: 12px;
+  }
+}
+</style>
